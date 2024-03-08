09116904851820d68ed0dace1d6d814bd60bc854
Rabbit <labcgood44@gmail.com>
2024-03-08 15:48:13 +0800

    Initial Commit

---
+++ Yahtzee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yahtzee
+//
+//  Created by Labe on 2024/3/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
